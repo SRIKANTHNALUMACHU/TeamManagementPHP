2686a5670570c88cffb359d368928c470feb0f53
SRIKANTHNALUMACHU <nalumachusrikanth1@gmail.com>
2021-03-04 22:02:03 +0530

    first code

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+</head>
+<body>
+<?php include 'nav/nav.php';?>
+<h1>hello</h1>
+</body>
+</html>
---
+++ nav/nav.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Navbar</title>
+<style type="text/css">
+*{
+    padding:0;
+    margin:0;
+}
+.navbar{
+    width:100%;
+    background:#000;
+}
+.navbar ul{
+    list-style-type:none;
+    text-align:center;
+}
+.navbar li{
+    display:inline-block;
+    color:#fff;
+    padding:10px;
+    margin-right:20px;
+}
+</style>
+</head>
+<body>
+<div class='navbar'>
+<ul>
+<li>Home</li>
+<li>Teams</li>
+<li>Tasks</li>
+<li>Login</li>
+</ul>
+</div>
+</body>
+</html>

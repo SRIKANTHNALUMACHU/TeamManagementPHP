ea24537e9c11fcb01e70d0edc024abf2e4d232e1
SRIKANTHNALUMACHU <nalumachusrikanth1@gmail.com>
2021-03-08 18:29:17 +0530

    loginpageUI

--- login/login.php
+++ login/login.php
@@ -4,26 +4,34 @@
 <title>login Page</title>
 <style type="text/css">
 body{
-    background-image:url("../image/login.jpg");
+    background-image:url("../image/4.jpg");
     background-color:#fff;
     align-items:center;
     font-family:sans-serif;
+    display:flex;
+    
 }
 .container{
 
+}
+h2{
+    text-align:center;
+    color:#ece2e1;
 }
 form{
     width:500px;
     border:2px solid #ccc;
     padding:30px;
-    background:#fff;
-    border-radius:15px;
+    background:#75cfb8;
+    background-image:url("../image/7.jpeg");
+    border-radius:25px;
     margin-top:250px;
     margin-left:500px;
     align-items:center;
+    
 }
 label{
-    color:#888;
+    color:#d3e0dc;
 }
 input{
     display:block;
@@ -32,12 +40,14 @@ input{
     margin-bottom:10px;
     width:90%;
     border-radius:5px;
+    
 }
 button{
     padding:10px;
     align:center;
-    margin-left:150px;
+    margin-left:200px;
     width:80px;
+    border-radius:5px;
 }
 </style>
 </head>
